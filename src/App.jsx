a3e641e87f48b66d2040d6568bfcf8c81a068d27
CooperHoag <cooperhoag@gmail.com>
2025-06-05 21:08:55 -0700

    Create initial function

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { episodeList } from "./data";
+import { useState } from "react";
+
 export default function App() {
   // TODO
+  const [episodesArray, setEpisodesArray] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState('');
+
+  const listOfEpisodes = () => {
+    return(
+      <section>
+        <h2>Episodes</h2>
+      </section>
+    )
+  }
+  
+  return(
+    <>
+      <h1>Dark Echoes</h1>
+      <main>
+        <listOfEpisodes />
+      </main>
+    </>
+  )
 }
